b9a96a93d891367dc7def78eb6951fefc5f16e58
vigneshwaran26 <vicky.ravi26@gmail.com>
2019-03-23 13:00:18 +0530

    smooth animation

--- app/src/main/java/com/example/listing/PersonAdapter.java
+++ app/src/main/java/com/example/listing/PersonAdapter.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -16,8 +18,16 @@ public class PersonAdapter extends ArrayAdapter<Person> {
     private static final String TAG = "PersonAdapter";
 
     private Context mContext;
-    int Mresource;
-    /**
+    private int Mresource;
+    private int lastPosition = -1;
+
+    private static class ViewHolder {
+        TextView name;
+        TextView birthday;
+        TextView gender;
+
+    }
+        /**
      * Default constructor for the PersonListAdapter
      * @param context
      * @param resource
@@ -30,7 +40,9 @@ public class PersonAdapter extends ArrayAdapter<Person> {
         Mresource = resource;
     }
 
-   //get person information
+
+
+        //get person information
     @NonNull
     @Override
     public View getView(int position,  View convertView,  ViewGroup parent) {
@@ -41,16 +53,37 @@ public class PersonAdapter extends ArrayAdapter<Person> {
 
         Person person = new Person(name,birthday,gender);
 
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        convertView = inflater.inflate(Mresource , parent , false);
+        //create view result for showing animation
+        final View result;
 
-        TextView tvname = (TextView) convertView.findViewById(R.id.textview1);
-        TextView tvBirthday = (TextView) convertView.findViewById(R.id.textView2);
-        TextView Gender = (TextView) convertView.findViewById(R.id.textview3);
+        //view holder object
+        ViewHolder holder;
 
-        tvname.setText(name);
-        tvBirthday.setText(birthday);
-        Gender.setText(gender);
+        if(convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(Mresource , parent , false);
+
+            holder= new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(R.id.textview1);
+            holder.birthday = (TextView) convertView.findViewById(R.id.textView2);
+            holder.gender = (TextView) convertView.findViewById(R.id.textview3);
+
+            result = convertView;
+
+            convertView.setTag(holder);
+
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+        Animation animation = AnimationUtils.loadAnimation(mContext ,
+                (position > lastPosition) ? R.anim.loading__down_anim : R.anim.loading_up_anim      );
+        result.startAnimation(animation);
+        lastPosition = position;
+
+        holder.name.setText(person.getName());
+        holder.birthday.setText(person.getBirthday());
+        holder.gender.setText(person.getGender());
 
 
         return convertView;
